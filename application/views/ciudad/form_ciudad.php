bf48e00cfd669b1766f1c134d7b1c3a97d4f87e9
rino7 <diego.a.olmedo@gmail.com>
2015-07-04 10:56:04 -0300

    archivos generados

---
+++ application/views/ciudad/form_ciudad.php
@@ -0,0 +1,8 @@
+<div class="col-md-5">
+    <form action="/ciudad/guardar" method="POST" class="form-vertical">
+        
+        <div class="form-group text-center">
+            <input type='submit' class="btn btn-primary" name='guardar' value='Guardar' />
+        </div>
+    </form>
+</div>
