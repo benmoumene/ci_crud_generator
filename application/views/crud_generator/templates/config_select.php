7cbb5a6f63eac6f6ffbad90486907c0b3b1e5046
rino7 <diego.a.olmedo@gmail.com>
2016-03-21 23:32:24 -0300

    Implementa config select

--- application/views/crud_generator/templates/config_select.php
+++ application/views/crud_generator/templates/config_select.php
@@ -10,15 +10,15 @@
                 <form id="form-config-option" class="form-inline">
                     <div class="form-group">
                         <label>Value</label><br>
-                        <input type="text" class="form-control" name="value_field" id="value_field" />
+                        <input type="text" style="width:100px" class="form-control" name="value_field" id="value_field" />
                     </div>
                     <div class="form-group">
                         <label>Text</label><br>
-                        <input type="text" class="form-control" name="text_field" id="text_field" />
+                        <input type="text"  style="width:100px" class="form-control" name="text_field" id="text_field" />
                     </div>
                     <div class="form-group">
                         <label>Posicion</label><br>
-                        <input type="number" placeholder="1" class="form-control" name="posicion" id="posicion" />
+                        <input type="number"  style="width:50px" placeholder="1" class="form-control" name="posicion" id="posicion" />
                     </div>
                     <div class="checkbox"><br>
                         <label>
@@ -29,13 +29,15 @@
                         <button class="btn btn-sm btn-success " id="btn-agregar-option" type="button"><?php echo glyphicon("plus"); ?></button>
                     </div>
                 </form>
+                <br>
+                <h4>Options agregadas</h4>
                 <div id="contenedor-options-select">
 
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn btn-primary" id="btn-guardar-cambios">Guardar cambios</button>
+                <button type="button" class="btn btn-primary js-btn-guardar-cambios">Guardar cambios</button>
             </div>
         </div>
     </div>
